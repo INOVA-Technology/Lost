6eaaf6034826898c5232c151d437db46dc6d5a93
Addisonbean <addisonbean@gmail.com>
2014-02-14 14:12:05 -0600

    Added rooms

--- item.rb
+++ item.rb
@@ -2,10 +2,11 @@ class Item
 
 	attr_accessor :name, :exam, :weight
 
-	def initialize name, exam, weight
+	def initialize name, exam, weight, hidden=false
 		@name = name
 		@exam = exam
 		@weight = weight
+		@hidden = hidden
 	end
 
 end
